8eedc8fc0fd7a4c7cf0671b2dee47c5002e74045
Thierry Reding <treding@nvidia.com>
2014-06-03 14:48:12 +0200

    drm/tegra: Implement more tiling modes
    Tegra124 supports a block-linear mode in addition to the regular pitch
    linear and tiled modes. Add support for these by moving the internal
    representation into a structure rather than a simple flag.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

--- drivers/gpu/drm/tegra/gem.h
+++ drivers/gpu/drm/tegra/gem.h
@@ -19,6 +19,17 @@
 #define TEGRA_BO_TILED     (1 << 0)
 #define TEGRA_BO_BOTTOM_UP (1 << 1)
 
+enum tegra_bo_tiling_mode {
+	TEGRA_BO_TILING_MODE_PITCH,
+	TEGRA_BO_TILING_MODE_TILED,
+	TEGRA_BO_TILING_MODE_BLOCK,
+};
+
+struct tegra_bo_tiling {
+	enum tegra_bo_tiling_mode mode;
+	unsigned long value;
+};
+
 struct tegra_bo {
 	struct drm_gem_object gem;
 	struct host1x_bo base;
@@ -26,6 +37,8 @@ struct tegra_bo {
 	struct sg_table *sgt;
 	dma_addr_t paddr;
 	void *vaddr;
+
+	struct tegra_bo_tiling tiling;
 };
 
 static inline struct tegra_bo *to_tegra_bo(struct drm_gem_object *gem)
